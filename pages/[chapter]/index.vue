a5847bb898421a464e0abda7369754165a47d4bf
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-13 08:31:27 -0400

    refactor: add margin to word grid

--- pages/[chapter]/index.vue
+++ pages/[chapter]/index.vue
@@ -1,11 +1,11 @@
 <template>
 	<div class="flex flex-col justify-center">
-		<div class="flex justify-center mt-32 mb-20">
+		<div class="flex justify-center mt-32">
 			<ContentDoc tag="article" class="prose prose-neutral !prose-invert" />
 		</div>
 
 		<div
-			class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10 md:gap-x-28 gap-x-20 mx-auto font-mono"
+			class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-y-10 md:gap-x-28 gap-x-20 mx-auto font-mono my-20"
 		>
 			<ContentQuery :path="$route.params.chapter.toString()" :skip="1" v-slot="{ data }">
 				<NuxtLink
