a0fc36db3ee4a78fcb181a81f5f3e4e1a2c61e32
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-15 22:43:17 -0400

    chore: deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,6 @@
-import { defineNuxtConfig } from "nuxt";
-
 export default defineNuxtConfig({
 	modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@vueuse/nuxt"],
-	css: ["@/assets/main.css"],
+	css: ["~/assets/main.css"],
 	typescript: {
 		shim: false,
 	},
