345e1feaa10177edc0dd14c21a95ea5542d9d1b6
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-18 16:15:25 -0400

    feat: add arrow key navigation

--- pages/[chapter]/[word].vue
+++ pages/[chapter]/[word].vue
@@ -4,4 +4,26 @@
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const route = useRoute();
+const router = useRouter();
+
+const $regex = new RegExp(`/${route.params.chapter}/`);
+
+const { data } = await useAsyncData(() => queryContent().where({ _path: { $regex } }).find());
+const words = data.value;
+
+const current = words.findIndex((page) => page.title === route.params.word);
+const prev = words[current - 1];
+const next = words[current + 1];
+
+onMounted(() => {
+	useEventListener(document, "keydown", (event) => {
+		if (event.key === "ArrowLeft" && prev) {
+			router.push(prev._path);
+		} else if (event.key === "ArrowRight" && next) {
+			router.push(next._path);
+		}
+	});
+});
+</script>
