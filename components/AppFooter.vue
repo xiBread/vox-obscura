77cc0d81fedef8b43166f0f36565092f1416174d
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-18 16:13:10 -0400

    feat: add home link to footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,7 +1,9 @@
 <template>
 	<footer class="p-8 font-mono text-sm">
-		<div class="flex items-center justify-center group text-neutral-500">
-			<NuxtLink to="all" class="group-hover:text-white">index</NuxtLink>
+		<div class="flex items-center justify-center text-neutral-500">
+			<NuxtLink to="/" class="hover:text-white">home</NuxtLink>
+			&nbsp;&bullet;&nbsp;
+			<NuxtLink to="all" class="hover:text-white">index</NuxtLink>
 		</div>
 	</footer>
 </template>
