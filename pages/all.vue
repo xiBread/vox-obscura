a853f14814310f1ef052e6c4445f1d887dc7c538
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-19 23:21:06 -0400

    feat: start index

--- pages/all.vue
+++ pages/all.vue
@@ -1,7 +1,20 @@
 <template>
-	<div></div>
+	<div class="font-mono text-neutral-400 my-32 flex items-center justify-center">
+		<ol class="[column-count:3] space-y-2 gap-x-32">
+			<li v-for="word in words" :key="word._path">
+				<NuxtLink :to="word._path" class="hover:text-white">{{ word.title }}</NuxtLink>
+			</li>
+		</ol>
+	</div>
 </template>
 
 <script setup lang="ts">
 useHead({ title: "Index" });
+const { navigation } = useContent();
+
+const words = navigation.value
+	.map((ch) => ch.children)
+	.flat()
+	.filter((w) => !w.description)
+	.sort((a, b) => a.title.localeCompare(b.title));
 </script>
