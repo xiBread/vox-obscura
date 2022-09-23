8a97fe18d09c991ce7d37785e979a99f40f1deef
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-23 17:29:39 -0400

    revert: use js for tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import("tailwindcss").Config}
+ */
+module.exports = {
+	plugins: [require("@tailwindcss/typography"), require("@headlessui/tailwindcss")],
+	content: ["./content/**/*.md"],
+	theme: {
+		fontFamily: {
+			mono: ["'Roboto Mono'", "monospace"],
+		},
+	},
+}
