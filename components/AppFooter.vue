65c613e02d4a5c48925afb9036d71e38596a762e
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-09-23 17:05:26 -0400

    refactor: use nav in footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,9 +1,9 @@
 <template>
 	<footer class="p-8 font-mono text-sm">
-		<div class="flex items-center justify-center text-neutral-500">
+		<nav class="flex items-center justify-center text-neutral-500">
 			<NuxtLink to="/" class="hover:text-white">home</NuxtLink>
 			&nbsp;&bullet;&nbsp;
-			<NuxtLink to="all" class="hover:text-white">index</NuxtLink>
-		</div>
+			<NuxtLink to="/all" class="hover:text-white">index</NuxtLink>
+		</nav>
 	</footer>
 </template>
