3d7365f5ae1c4db27e710161fdae59714db89f09
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-10-01 16:46:30 -0400

    revert: remove search

--- components/Search.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div>
-		<input type="text" />
-	</div>
-</template>
-
-<script setup lang="ts">
-const { result, search } = useAlgoliaSearch("vox_obscura");
-</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/algolia", "@nuxtjs/tailwindcss", "@nuxt/content", "@vueuse/nuxt"],
+	modules: ["@nuxtjs/tailwindcss", "@nuxt/content", "@vueuse/nuxt"],
 	css: ["~/assets/main.css"],
 	nitro: {
 		plugins: ["~/server/plugins/content.ts"],
@@ -7,10 +7,6 @@ export default defineNuxtConfig({
 	typescript: {
 		shim: false,
 		strict: true,
-	},
-	algolia: {
-		apiKey: "b82ae5e1e5474f93e7e72d56b4a735a8",
-		applicationId: "4V3K71ASE8",
 	},
 	content: {
 		documentDriven: true,
