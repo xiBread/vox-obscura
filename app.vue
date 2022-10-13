ce1669cdd5cef18d19afeec97731a418a4f2e9b9
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-10-12 23:35:36 -0400

    refactor: move footer to app

--- app.vue
+++ app.vue
@@ -11,7 +11,13 @@
 				<NuxtPage />
 			</main>
 
-			<AppFooter />
+			<footer class="p-8 font-mono text-sm">
+				<nav class="flex items-center justify-center text-neutral-500">
+					<NuxtLink to="/" class="hover:text-white">home</NuxtLink>
+					&nbsp;&bullet;&nbsp;
+					<NuxtLink to="/all" class="hover:text-white">index</NuxtLink>
+				</nav>
+			</footer>
 		</div>
 	</div>
 </template>
--- components/AppFooter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<footer class="p-8 font-mono text-sm">
-		<nav class="flex items-center justify-center text-neutral-500">
-			<NuxtLink to="/" class="hover:text-white">home</NuxtLink>
-			&nbsp;&bullet;&nbsp;
-			<NuxtLink to="/all" class="hover:text-white">index</NuxtLink>
-		</nav>
-	</footer>
-</template>
