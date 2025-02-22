1fce736250f9f3c8b17652c0dde6290e7ffdf5c3
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-02-21 21:13:11 -0500

    feat: seo/og

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
-
+import fs from "node:fs/promises";
 import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
 	integrations: [svelte()],
+	vite: {
+		plugins: [
+			tailwindcss(),
+			{
+				name: "vite-plugin-fonts",
+				async transform(code: string, id: string) {
+					if (id.endsWith(".ttf") || id.endsWith(".woff")) {
+						const buffer = await fs.readFile(id);
+
+						return {
+							code: `export default ${JSON.stringify(buffer)}`,
+						};
+					}
+				},
+			},
+		],
+	},
 });
---
+++ src/components/OgTemplate.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { dictionary } from "../lib/dictionary";
+	import { romanNumerals } from "../lib/util";
+
+	interface Props {
+		chapter: string;
+		word: string | null;
+	}
+
+	const props: Props = $props();
+
+	const chapter = dictionary.chapters[props.chapter];
+	const word = props.word ? dictionary.get(props.word) : null;
+</script>
+
+{#if word}
+	<!--  -->
+{:else}
+	<div class="flex items-center justify-center w-full h-full text-white">
+		<img
+			class="absolute w-full h-full"
+			src={`${import.meta.env.PUBLIC_BASE_URL}/og-background.jpg`}
+			alt=""
+		/>
+
+		<hgroup class="flex flex-col">
+			<span class="text-3xl" style="font-family: Playfair;">
+				{romanNumerals[chapter.id]}
+			</span>
+
+			<h1 class="font-black my-1 text-8xl">
+				{chapter.name.toUpperCase()}
+			</h1>
+
+			<p class="text-2xl leading-0">{chapter.description}</p>
+		</hgroup>
+	</div>
+{/if}
