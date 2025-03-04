014eddadd2bdc7eaea7711267b87ff39a5f9ea65
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2025-03-03 21:14:08 -0500

    feat: background variants

--- src/components/OgTemplate.svelte
+++ src/components/OgTemplate.svelte
@@ -11,12 +11,16 @@
 
 	const chapter = dictionary.chapters[props.chapter];
 	const word = props.word ? dictionary.get(props.word) : null;
+
+	const bgVariant = Math.floor(Math.random() * 4 + 1);
 </script>
 
 <img
 	class="absolute w-full h-full"
-	src={`${import.meta.env.PUBLIC_BASE_URL}/og-background.jpg`}
+	src="{import.meta.env.PUBLIC_BASE_URL}/og-background-{bgVariant}.jpg"
 	alt=""
+	width="1200"
+	height="630"
 />
 
 {#if word}
